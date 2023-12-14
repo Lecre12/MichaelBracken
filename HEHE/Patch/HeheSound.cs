31c5e6ef14c65cb0b2a3103b74ec4b317e8270eb
Adrián <124732460+Lecre12@users.noreply.github.com>
2023-12-15 00:19:20 +0100

    Agregar archivos de proyecto.

---
+++ HEHE/Patch/HeheSound.cs
@@ -0,0 +1,83 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace HEHE.Patch
+{
+    [HarmonyPatch(typeof(FlowermanAI))]
+    internal class HeheSound
+    {
+        static bool done = false;
+        static AudioClip newSFX;
+        static ManualLogSource mls;
+        
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void prepareAudio()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("Lecre.conchaSuMareMod");
+            // Load the audio file
+            string location = ((BaseUnityPlugin)HeheBase.instance).Info.Location;
+            string text = "HEHE.dll";
+            string text2 = location.TrimEnd(text.ToCharArray());
+            string path = text2 + "HEHE.wav";
+            ((MonoBehaviour)HeheBase.instance).StartCoroutine(LoadAudio("file:///" + path, clip =>
+            {
+                newSFX = clip;
+            }));
+        }
+
+        static IEnumerator LoadAudio(string url, Action<AudioClip> callback)
+        {
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.WAV))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    mls.LogError("Failed to load audio assets!");
+                }
+                else
+                {
+                    AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
+                    if (clip == null)
+                    {
+                        mls.LogError("The audio clip is null after loading!");
+                    }
+                    else
+                    {
+                        callback(clip);
+                        mls.LogInfo("Audio inserted");
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void FlowerManAudioKillPatch(ref FlowermanAI __instance)
+        {
+            FlowermanAI flowerMan = __instance;
+            if (flowerMan.inKillAnimation && !done)
+            {
+                done = true;
+                AudioSource audioSource = flowerMan.gameObject.AddComponent<AudioSource>();
+                audioSource.clip = newSFX;
+                audioSource.Play();
+                mls.LogInfo("The FlowerMan got someone HEHE");
+            }
+            if (!flowerMan.inKillAnimation)
+            {
+                done = false;
+            }
+        }
+        
+    }
+}
---
+++ HEHE/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using HEHE.Patch;
+
+namespace HEHE
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class HeheBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Lecre.HeheMod";
+        private const string modName = "LC HEHEMod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static HeheBase instance;
+
+        internal ManualLogSource mls;
+
+
+        void Awake()
+        {
+
+            if (instance == null)
+            {
+                instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Lecre.ConchaSuMareMod is loading");
+
+            harmony.PatchAll(typeof(HeheBase));
+            harmony.PatchAll(typeof(HeheSound));
+        }
+
+    }
+}
